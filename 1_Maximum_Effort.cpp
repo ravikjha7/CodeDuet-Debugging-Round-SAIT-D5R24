50e8ab8164407c237622b855598dbf699ae4a2ed
Ravi Jha <73632663+ravikjha7@users.noreply.github.com>
2023-04-01 17:32:40 +0530

    D5R24 Question 1 Done

--- 1_Maximum_Effort.cpp
+++ 1_Maximum_Effort.cpp
@@ -8,18 +8,19 @@
  * @param size the number of elements in the array
  *
  */
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
-int maxValue(int *arr, int size)
+int maxValue(int arr[], int size)
 {
-    int *max = arr;
+    int max = arr[0];
     for (int i = 1; i < size; i++)
     {
-        if (*arr > *max)
+        if (arr[i] > max)
         {
-            max = arr;
+            max = arr[i];
         }
-        arr++;
+        // arr++;
     }
     return max;
 }
@@ -27,7 +28,7 @@ int maxValue(int *arr, int size)
 int main()
 {
     int arr[] = {10, 20, 5, 30, 15};
-    int size = sizeof(arr) / sizeof(arr);
+    int size = sizeof(arr) / sizeof(arr[0]);
 
     int max = maxValue(arr, size);
 
